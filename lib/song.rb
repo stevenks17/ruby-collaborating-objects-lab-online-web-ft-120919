970a24b43cda0442c735fea0571ac135686acb6f
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-07 17:28:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,10 @@ class Song
     artist.add_song(song)
   end
   
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+  
 end
