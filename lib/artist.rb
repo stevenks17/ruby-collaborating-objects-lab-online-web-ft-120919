395e37c8856814a3b1b1dadbda5549e12bed2e36
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-07 17:17:39 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,58 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+ 
+ 
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+  
+   def self.all
+     @@all
+   end 
+
+ 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    song
+  end
+  
+  def songs
+    @songs
+  end
+ 
+  def save
+   @@all << self
+  end
+  
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+ 
+  def self.find_or_create_by_name(name)   
+    
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+ 
+   def songs
+    Song.all.select {|song| song.artist == self}
+   end
+
+   def print_songs 
+    self.songs.each {|song| song.name}
+   end
+end
---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  attr_accessor :artist, :name
+   @@all = []
+ 
+ 
+  def initialize(name)
+    @name = name
+    save
+  end
+ 
+ def save
+    @@all << self
+ end  
+ 
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end 
+  
+  def self.all
+     @@all
+  end 
+  
+  def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
+  
+end
