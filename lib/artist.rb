2c4b480041a58a761803e8268306ef29e87a95cb
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-07 17:28:42 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -52,7 +52,8 @@ class Artist
     Song.all.select {|song| song.artist == self}
    end
 
-   def print_songs 
-    self.songs.each {|song| song.name}
-   end
+  def print_songs
+    songs.each { |song| puts song.name }
+  end
+
 end
